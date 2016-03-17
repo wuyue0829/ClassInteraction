d43a64bd1030955debc5e2ce3b7a8dd8c9773139
wuyue <yy107829@sina.com>
2016-03-17 18:01:37 +0800

    初步框架

--- app/src/main/java/com/cy/classinteraction/MainActivity.java
+++ app/src/main/java/com/cy/classinteraction/MainActivity.java
@@ -1,37 +1,43 @@
 package com.cy.classinteraction;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.util.TimerTask;
+
+/**
+ * 应用起始页面
+ * 将在这个页面完成以下的操作
+ * 1、设置点击进入页面时白屏几秒钟的画面；（好多的应用并没有处理开始界面白屏的问题，故这个地方可以先不考虑）
+ * 2、判断是否第一次进入程序，做相对应的操作；
+ * 3、获取屏幕的宽和高写入到配置文件
+ */
 public class MainActivity extends AppCompatActivity {
 
+
+    private ViewPager vp_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    /**
+     * 写一个定时器在几秒后执行跳转操作
+     */
+    class Task extends TimerTask{
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        @Override
+        public void run() {
+            DisplayMetrics metric = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(metric);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
         }
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/cy/classinteraction/util/SysConfig.java
@@ -0,0 +1,8 @@
+package com.cy.classinteraction.util;
+
+/**
+ * Created by wuyue on 2016/3/17.
+ */
+public class SysConfig {
+
+}
