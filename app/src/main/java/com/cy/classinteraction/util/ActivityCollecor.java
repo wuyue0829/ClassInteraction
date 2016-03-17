8ef37404f531e6328d76691b83c63e7f140118c4
wuyue <yy107829@sina.com>
2016-03-17 17:22:47 +0800

    初步框架

--- app/src/main/java/com/cy/classinteraction/util/ActivityCollecor.java
+++ app/src/main/java/com/cy/classinteraction/util/ActivityCollecor.java
@@ -7,16 +7,23 @@ import android.app.Activity;
 
 
 public class ActivityCollecor {
+
+	//初始化Activity list
 	public static List<Activity> acticities = new ArrayList<Activity>();
 
+	//添加activity到list中
 	public static void addActivity(Activity activity){
 		acticities.add(activity);
 	}
 
+	//从当前list中删除activity
 	public static void removeActivity(Activity activity){
 		acticities.remove(activity);
 	}
 
+	/**
+	 * 关闭所有的activity，用于退出程序时使用
+	 */
 	public static void finishAll(){
 		for(Activity activity2:acticities){
 			if(! activity2.isFinishing()){
