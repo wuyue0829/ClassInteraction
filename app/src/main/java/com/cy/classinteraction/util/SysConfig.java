3dcaf13a701549a4144c0a8ecad370acabe84bcf
wuyue <yy107829@sina.com>
2016-03-18 15:28:09 +0800

    初步框架

--- app/src/main/java/com/cy/classinteraction/util/SysConfig.java
+++ app/src/main/java/com/cy/classinteraction/util/SysConfig.java
@@ -1,8 +1,33 @@
 package com.cy.classinteraction.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by wuyue on 2016/3/17.
+ * 存放各种系统参数
  */
 public class SysConfig {
+
+    private final static String SYS_PAMS = "";
+    private SharedPreferences config;
+    private static SysConfig sysConfig = null;
+
+    private SysConfig(Context context){
+        config = context.getSharedPreferences(SYS_PAMS,0);
+    }
+
+    /**
+     * 单例模式
+     * @param context
+     * @return
+     */
+    private final static SysConfig getSysConfig(Context context){
+        if(sysConfig == null){
+            sysConfig = new SysConfig(context);
+        }
+        return sysConfig;
+    }
+
 
 }
